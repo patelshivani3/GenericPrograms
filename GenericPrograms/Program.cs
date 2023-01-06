5155357fa0de940cfd411b8713cdaf296aaeaec9
patelshivani3 <shivpatel3001@gmail.com>
2023-01-06 18:42:19 +0530

    Adding Welcome Message to Generic Programs

---
+++ GenericPrograms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to Generic Programs");
+            Console.ReadLine();
+        }
+    }
+}
