36d66018010a3d9b747b1cd4cb13a9c3283c5acb
patelshivani3 <shivpatel3001@gmail.com>
2023-01-06 19:50:48 +0530

    Adding Maximum floating Number

--- GenericPrograms/MaxNumber.cs
+++ GenericPrograms/MaxNumber.cs
@@ -22,5 +22,19 @@ namespace GenericPrograms
                 Console.WriteLine("The Maximum Number is : "+c);
 
         }
+        public static void Number1()
+        {
+            float a = 3.2f;
+            float b = 6.8f;
+            float c = 5.5f;
+            Console.WriteLine("Find Maximum Number From ( 3.2 , 6.8 , 5.5)");
+            if ((a.CompareTo(b) > 0) && (a.CompareTo(c) > 0))
+                Console.WriteLine("The maximum Number is : " + a);
+            else if ((b.CompareTo(a) > 0) && (b.CompareTo(c) > 0))
+                Console.WriteLine("The Maximum Number is : " + b);
+            else
+                Console.WriteLine("The Maximum Number is : " + c);
+
+        }
     }
 }
--- GenericPrograms/Program.cs
+++ GenericPrograms/Program.cs
@@ -11,13 +11,17 @@ namespace GenericPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Generic Programs");
-            Console.WriteLine("\n1.Maximum Integer");
+            Console.WriteLine("\n1.Maximum Integer Number\n2.Maximum Float Number");
             int option = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("--------------------------------------");
             switch(option)
             {
                 case 1:
                     MaxNumber.Number();
+                    break;
+
+                case 2:
+                    MaxNumber.Number1();
                     break;
             }
             Console.ReadLine();
